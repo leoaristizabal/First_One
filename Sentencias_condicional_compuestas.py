7900c805c6b98e69adec386ee332f796bf23edf3
leoaristizabal <68977686+leoaristizabal@users.noreply.github.com>
2020-07-30 21:32:58 -0400

    Cambio en el texto

--- Sentencias_condicional_compuestas.py
+++ Sentencias_condicional_compuestas.py
@@ -1,21 +1,18 @@
-print('Calcula tu promedio pav perrito: ')
+print('Sistema para calcular el promedio de notas en tres materias: ')
 nombre = input('Para comenzar, cual es tu nombre?: ')
-nota_mate = float(input(nombre +', Cual es tu nota en mate llae?: '))
-nota_che = float(input(nombre + ', cual es tu nota en quimica llae?: '))
-nota_bio = float(input(nombre + ', cual es tu nota en biologia mano?: '))
+nota_mate = float(input(nombre +', Cual es tu nota en math?: '))
+nota_che = float(input(nombre + ', cual es tu nota en química?: '))
+nota_bio = float(input(nombre + ', cual es tu nota en biología?: '))
 
-print('''verga esto esta arrecho de calcular manaure, pero tu confia.
-Vamos a ver qlq''')
+print('Wait a few seconds')
 
 promedio = (nota_mate + nota_che + nota_bio)/3
-print('Aja mano tu promedio fue de ', round(promedio,2))
+print('Tu promedio fue de: ', round(promedio,2))
 
-
-if promedio >=10:
-    print('''Perrito lo lograste llae!!!
-Coronamos''')
+if promedio >=5:
+    print("Lo lograste")
 else:
-    print('''Verga mano, no hay manera.
-Estas clavadisimo''')
+    print('''
+Has reprobado''')
 
-print('Dejalo asi mano, dale por ahi')
+print('Sigue intentando')
