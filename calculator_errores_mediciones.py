4f7d8a5985bfccc79cf25ac1ea85e08a5eb215a4
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2020-08-01 19:19:48 -0400

    Primer codigo calculadora de errores en mediciones y otros archivos de practica.

---
+++ calculator_errores_mediciones.py
@@ -0,0 +1,59 @@
+print('===========================================================')
+print('Calculador de Desviacion Estandar en un grupo de mediciones')
+print('=========================================================== \n')
+
+print(' \n Ingresa un maximo de cuatro valores:')
+print('======================================== \n')
+
+opcion_uno = float(input('Ingresa la primera medicion: '))
+opcion_dos = float(input('Ingresa la segunda medicion: '))
+opcion_tres = float(input('Ingresa la tercera medicion: '))
+opcion_cuatro = float(input('Ingresa la cuarta medicion: '))
+
+
+print('\n Menu de opciones')
+print('================ \n')
+
+print('Presiona 1 para calcular el Valor Medio.')
+print('Presiona 2 para calcular la Desviacion Estandar.')
+print('Presiona 3 para calcular Error Relativo.')
+print('Presiona 4 para calcular el Error Porcentual.')
+
+opcion = int(input('\n Indique la opcion deseada: '))
+
+if opcion == 1:
+       valor_medio = opcion_uno + opcion_dos + opcion_tres + opcion_cuatro #Ver Formula valor medio (esta es un ejemplo)!
+       print('El valor medio de sus mediciones es: ', valor_medio)
+       
+elif opcion == 2:
+    desv_stnd = (opcion_uno + opcion_dos + opcion_tres + opcion_cuatro)*opcion_uno
+    print('La Desviacion Estandar de sus mediciones es: ', desv_stnd)
+elif opcion == 3:
+    error_rel = (opcion_uno + opcion_dos + opcion_tres + opcion_cuatro)*opcion_dos
+    print('El Error Relativo de sus mediciones es: ', error_rel)
+elif opcion == 4:
+    error_porc = (opcion_uno + opcion_dos + opcion_tres + opcion_cuatro)*opcion_tres
+    print('El Error Porcentual de sus mediciones es: ', error_porc, '%')
+else:
+    print('OPCION NO VALIDA')
+print('fin')    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ converter_SCA.py
@@ -0,0 +1,72 @@
+# Sentencias condicionales anidadas.}
+
+print('=========')
+print('Conversor')
+print('=========\n')
+
+print('Menu de Opciones: ')
+print('================= \n')
+
+print('Presiona 1 para convertir de numero a palabra.')
+print('Presiona 2 para convertir de palabra a numero. \n')
+
+opcion = int(input('Indica la opcion deseada: '))
+
+if opcion == 1:
+    print('\n Conversor de numero a palabra. \n')
+    opcion_uno = int(input('Indica el numero a convertir y presiona Enter: '))
+
+    if opcion_uno == 1:
+        print('El numero es "UNO"')
+    elif opcion_uno == 2:
+        print('El numero es "DOS"')
+    elif opcion_uno == 3:
+        print('El numero es "TRES"')
+    elif opcion_uno == 4:
+        print('El numero es "CUATRO"')
+    elif opcion_uno == 5:
+        print('El numero es "CINCO"')
+    elif opcion_uno == 0:
+        print('El numero es "CERO"')
+    else:
+        print('El numero seleccionado no esta registrado')
+
+elif opcion == 2:
+    print('\n Conversor de palabra a numero. \n')
+    
+    opcion_dos = input('\n Ingrese la palabra a convertir y presiona Enter: \n')
+    
+    if opcion_dos == 'uno':
+        print('El numero es "1"')
+    elif opcion_dos == 'dos':
+        print('El numero es "2"')
+    elif opcion_dos == 'tres':
+        print('El numero es "3"')
+    elif opcion_dos == 'cuatro':
+        print('El numero es "4"')
+    elif opcion_dos == 'cinco':
+        print('El numero es "5"')
+    elif opcion_dos == 'cero':
+        print('El numero es "0"')
+    else:
+        print('La palabra seleccionada no esta registrada')
+    
+     
+else:
+    print('Opcion no valida')
+print('Fin.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
