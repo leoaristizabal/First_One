b0442ac18ca11ac5cb17520832ec94c8fd1b967c
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2020-08-04 12:12:46 -0400

    Primera prueba de operadores de asignación con un programa que incrementa y disminuye una varaible.

---
+++ operadores_asignacion_2.py
@@ -0,0 +1,24 @@
+nombre = 'Hola '
+nombre += input('Escribe tu nombre: ')
+
+print(nombre,', examinaremos el incremento y disminucion de una variable. \n')
+
+print('Incremento o aumento: ')
+x=1
+print('El valor inicial de x es: ',x)
+x+= 1   #Aqui la variable X ya vale 2 y asi sucesivamente
+x+= 1
+x+= 1
+x+= 1
+print('El valor final de x es: ', x, '\n')
+
+print('Disminución: ')
+print('El valor inicial de X es: ', x)
+#Aca la variable X vale 5
+
+x-=1
+x-=1
+x-=1
+x-=1
+print('El valor final de X es: ', x, '\n')
+
