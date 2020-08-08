94fe36fa827bc51d7a265d2a3c8a43504b0d1dd9
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2020-08-07 21:21:10 -0400

    shor madlips with a poem

---
+++ shor_madlips.py
@@ -0,0 +1,11 @@
+print('MAD LIPS GAME WITH A SHORT POEM')
+print('******************************* \n')
+color = input('Enter a color: ')
+pluralnoun = input('Enter a plural noun: ')
+celebrity = input('Enter a celebrity: ')
+
+print('Rose are '+ color)
+print(pluralnoun +  ' are blue')
+print('I love ' + celebrity)
+
+
