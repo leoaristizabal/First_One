5b25875e5dacc1b1a4657b9f780e25f880e9b825
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2020-08-08 21:56:44 -0400

    Primer encuentro con funciones, parametros y tuples

---
+++ tuples_n_functions.py
@@ -0,0 +1,10 @@
+coordinates = (4,5)
+print(coordinates[0])
+
+def say_hi (name, age):
+    print("Hello " + name +", you are " + str(age))
+
+print("Principio")
+say_hi(" Leo", 25)
+say_hi(" Mike", 23)
+print('Ultimo')
