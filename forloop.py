f10ffefcb67902543f938eaec3d225f554b362e1
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2020-08-21 20:50:42 -0400

    introduction to for loops

---
+++ forloop.py
@@ -0,0 +1,20 @@
+
+#First one
+
+for letter in "Giraffe Academy":
+    print(letter)
+
+
+#sec
+friends = ["Fran", "Dario","Raul","Karen"]
+for friend in friends:
+    print(friend)
+
+#Terc - Numbers
+
+for index in range(10):
+    print(index)
+
+for index1 in range(3,10):
+    print(index1)
+
