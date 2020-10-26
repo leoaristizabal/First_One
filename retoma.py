0ceeb0fa718c60307e6ed6bb9bd64cea30f281b5
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2020-10-26 18:29:33 -0400

    taking python out again

---
+++ retoma.py
@@ -0,0 +1,9 @@
+#Know your errir messages 
+current_time_str = input("What is the current time (0-23h):")
+wait_time_str = input("How many hours do you want to wait")
+
+current_time_int = int(current_time_str)
+wait_time_int = int(wait_time_str)
+
+final_time_int = current_time_int + wait_time_int
+print(final_time_int)
